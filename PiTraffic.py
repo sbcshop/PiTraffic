13c9239195f3e8798f559d3013736ac497630895
sbcshop <gajender.singh@sb-components.co.uk>
2017-07-03 22:20:15 +0100

    Add files via upload

---
+++ PiTraffic.py
@@ -0,0 +1,69 @@
+#!/usr/bin/python
+
+# Library for PiTraffic
+# Developed by: SB Components
+# Author: Ankur
+# Project: PiTraffic
+# Python: 3.4.2
+
+import RPi.GPIO as GPIO
+import time
+
+GPIO.setmode(GPIO.BOARD)
+GPIO.setwarnings(False)
+
+class Buzzer:
+    def __init__(self):
+        self.pin = 12
+        GPIO.setup(self.pin,GPIO.OUT)
+        GPIO.output(self.pin, GPIO.LOW)
+
+    def on(self):
+        print("Buzzer - ON")
+        GPIO.output(self.pin,GPIO.HIGH)
+
+    def off(self):
+        print("Buzzer - OFF")
+        GPIO.output(self.pin,GPIO.LOW)       
+    
+
+class Traffic:
+    ''' Class to handle LED's
+
+    Arguments:
+    direction =  (i.e. "EAST","WEST","NORTH","SOUTH")
+    color = Color of LED
+    '''
+    traffic_pins = {"SOUTH":{'RED':11,'YELLOW':13, "GREEN":15},
+                    "WEST" :{'RED':16,'YELLOW':18, "GREEN":22},
+                    "NORTH":{'RED':29,'YELLOW':31, "GREEN":33},
+                    "EAST" :{'RED':36,'YELLOW':38, "GREEN":40}}
+
+
+    def __init__(self, direction, color):
+        self.pin = self.traffic_pins[direction][color]
+        self.direction = direction
+        self.color = color
+        GPIO.setup(self.pin,GPIO.OUT)
+        GPIO.output(self.pin, GPIO.LOW)
+
+    def on(self):
+        print(self.direction + " " + self.color + " - ON")
+        GPIO.output(self.pin,GPIO.HIGH)
+
+    def off(self):
+        print(self.direction + " " + self.color + " - OFF")
+        GPIO.output(self.pin,GPIO.LOW)
+
+class Button:
+    Pressed = False
+
+    def __init__(self):
+        self.pin = 7
+        GPIO.setup(self.pin,GPIO.IN, pull_up_down=GPIO.PUD_UP)
+        
+    def press(self):
+        input_state = GPIO.input(self.pin)
+        if input_state == False:
+            print("Button - Pressed")
+            self.Pressed = True
---
+++ TrafficTest.py
@@ -0,0 +1,78 @@
+import PiTraffic
+import time
+
+SouthRed = PiTraffic.Traffic("SOUTH", "RED")
+SouthYellow = PiTraffic.Traffic("SOUTH", "YELLOW")
+SouthGreen = PiTraffic.Traffic("SOUTH", "GREEN")
+
+
+EastRed = PiTraffic.Traffic("EAST", "RED")
+EastYellow = PiTraffic.Traffic("EAST", "YELLOW")
+EastGreen = PiTraffic.Traffic("EAST", "GREEN")
+
+NorthRed = PiTraffic.Traffic("NORTH", "RED")
+NorthYellow = PiTraffic.Traffic("NORTH", "YELLOW")
+NorthGreen = PiTraffic.Traffic("NORTH", "GREEN")
+
+
+WestRed = PiTraffic.Traffic("WEST", "RED")
+WestYellow = PiTraffic.Traffic("WEST", "YELLOW")
+WestGreen = PiTraffic.Traffic("WEST", "GREEN")
+
+Buzz = PiTraffic.Buzzer()
+
+# All direction RED LED ON
+def AllRed():
+    SouthRed.on()
+    EastRed.on()
+    NorthRed.on()
+    WestRed.on()
+
+AllRed()
+       
+while True:
+    Buzz.on()
+    time.sleep(0.2)
+    Buzz.off()
+
+    EastRed.off()
+    EastYellow.on()
+    time.sleep(1)
+    EastYellow.off()
+    EastGreen.on()
+    time.sleep(2)
+    EastGreen.off()
+    EastRed.on()
+    time.sleep(1)
+ 
+    WestRed.off()
+    WestYellow.on()
+    time.sleep(1)
+    WestYellow.off()
+    WestGreen.on()
+    time.sleep(2)
+    WestGreen.off()
+    WestRed.on()
+    time.sleep(1)
+
+    NorthRed.off()
+    NorthYellow.on()
+    time.sleep(1)
+    NorthYellow.off()
+    NorthGreen.on()
+    time.sleep(2)
+    NorthGreen.off()
+    NorthRed.on()
+    time.sleep(1)
+
+    SouthRed.off()
+    SouthYellow.on()
+    time.sleep(1)
+    SouthYellow.off()
+    SouthGreen.on()
+    time.sleep(2)
+    SouthGreen.off()
+    SouthRed.on()
+    time.sleep(1)
+    
+ 
